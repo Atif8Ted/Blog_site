3cf1849c9735fd974e47a50ac674ad2beefbbe5b
Atif8ted <mohd.atifimam@hotmail.com>
2017-11-08 04:29:19 +0530

    made changes

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from django.contrib.auth import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blogs.urls')),
+    url(r'accounts/login/$', views.logout, name='logout', kwargs={'next_page': '/'})
+
 ]
