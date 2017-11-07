f86cbe56fc26cb737c6ff1e31bfb35a1c8f57f0c
Atif8ted <mohd.atifimam@hotmail.com>
2017-11-07 21:00:24 +0530

    updated

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -17,6 +17,9 @@ class Post(models.Model):
     def approve_comments(self):  # approved comments to publish
         return self.comments.filter(approved_comments=True)
 
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs={'pk': self.pk})
+
     def __str__(self):
         return self.title
 
@@ -30,8 +33,10 @@ class Comment(models.Model):
 
     def approve(self):
         self.approved_comment = True
-        slef.save()
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse('post_list')
 
     def __str__(self):
         return self.text
-
