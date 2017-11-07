bc9868d246a8cae7e6d59cd37b371f7d1fa4f1d8
Atif8ted <mohd.atifimam@hotmail.com>
2017-11-07 17:51:33 +0530

    updated

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -26,6 +26,12 @@ class Comment(models.Model):
     author = models.CharField(max_length=200)
     text = models.TextField()
     create_date = models.DateTimeField(default=timezone.now())
-    approved_comment = models.BooleanField(default=True)
+    approved_comment = models.BooleanField(default=False)
 
-    
+    def approve(self):
+        self.approved_comment = True
+        slef.save()
+
+    def __str__(self):
+        return self.text
+
